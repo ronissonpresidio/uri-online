bced8df0927b0344e7d7c466db43ff203059d8b8
ronissonpresidio <ronisson.dev@gmail.com>
2023-02-26 23:56:29 -0300

    Resolucao do exercicio 1001

---
+++ Uri1001/src/Uri1001.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Uri1001 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int soma = a + b;
+
+        System.out.println("X = " + soma);
+
+        sc.close();
+    }
+}
