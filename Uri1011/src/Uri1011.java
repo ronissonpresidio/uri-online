ec2904552a537159805c62e817f3186bf72df2bf
ronissonpresidio <ronisson.dev@gmail.com>
2023-03-04 21:31:22 -0300

    Resolucao do exercicio 1011

---
+++ Uri1011/src/Uri1011.java
@@ -0,0 +1,23 @@
+import java.lang.invoke.VolatileCallSite;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Uri1011 {
+    public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.US);
+
+        Scanner sc = new Scanner(System.in);
+
+        double raio, volume;
+
+        raio = sc.nextDouble();
+
+        volume = (4.0 / 3) * 3.14159 * Math.pow(raio, 3);
+
+        String valorFormatado = String.format("%.3f", volume);
+
+        System.out.println("VOLUME = " + valorFormatado);
+
+        sc.close();
+    }
+}
